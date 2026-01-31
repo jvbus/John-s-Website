88c065dc66bfd0a2944901844cb80395f7084c50
John Ross <jross@dpthermoforming.com>
2026-01-31 15:36:44 -0500

    added title animation

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const button =
-document.getElementById("btn");
 const title =
 document.getElementById("title");
 
-button.addEventListener("click", () => {
-  title.textContent = "You clicked the button";
-});
+let scale = 1;
+let growing = true;
+
+function animate() {
+  if (growing) {
+    scale += 0.005;
+    if (scale >= 1.05) growing = false;
+  } else {
+    scale -= 0.005;
+    if (scale <= 1.0) growing = true;
+  }
+
+  title.style.transform = 'scale($
+{scale})';
+  requestAnimationFrame(animate);
+}
+
+animate();
+
